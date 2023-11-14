a2a29587ac9ecdc729e600a3ccd09aa8856e189a
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2023-11-14 11:57:47 +0100

    hello world

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.scss'
+
+const Home = () => {
+  return (
+    <div className={styles.hello}>
+      <h1>JSConf Budapest <br/> 2024</h1>
+
+      <p>June 27-28 2024, Budapest, Hungary</p>
+    </div>
+  )
+}
+
+export default Home
