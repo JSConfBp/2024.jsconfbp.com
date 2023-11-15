26828abf7de46ba874b6adf1a172a2d6c244310d
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2023-11-15 16:54:06 +0100

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from './index.module.scss'
 
 
@@ -5,6 +6,9 @@ import styles from './index.module.scss'
 const Home = () => {
   return (
     <div className={styles.hello}>
+      <Head>
+        <title>JSConf Budapest 2024</title>
+      </Head>
       <h1>JSConf Budapest <br/> 2024</h1>
 
       <p>June 27-28 2024, Budapest, Hungary</p>
