3488a09d00988a103794de1c2acdb7a3d690795b
Greg Nagy <greg@dhamma.works>
2023-11-19 15:15:39 +0100

    add WideImage component

---
+++ ui/wide-image.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './wide-image.module.scss'
+import Image from 'next/image'
+
+const WideImage = ({ src, alt = '' }) => (
+  <div className={styles.wideimage}>
+    <Image
+      fill
+      src={`/${src}`}
+      alt={alt}
+      className={styles.image}
+    />
+  </div>
+)
+
+export default WideImage
