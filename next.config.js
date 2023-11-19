a9690a321baa6b3db78e2f7c2e462f5a029b625f
Greg Nagy <greg@dhamma.works>
2023-11-19 15:14:40 +0100

    add mdx dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+// https://nextjs.org/docs/pages/building-your-application/configuring/mdx
+const withMDX = require("@next/mdx")();
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
 
-module.exports = withPWA({
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+// Example mdx and pwa config together: https://dev.to/j471n/convert-nextjs-app-to-pwa-3fd#comment-247la
+module.exports = withPWA(withMDX(nextConfig));
