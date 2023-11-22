f4e69652e84e95b9afe48dbf25aeca3ed38ed5e6
Greg Nagy <greg@dhamma.works>
2023-11-22 17:17:05 +0100

    add the fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "../styles/global.scss";
+import "../global.scss";
+
+import { JetBrains_Mono, Shrikhand } from "next/font/google";
+
+const jetbrains_mono = JetBrains_Mono({
+  subsets: ["latin-ext"],
+  fallback: "monospace",
+  variable: "--font-jetbrains-mono",
+});
+
+const shrikhand = Shrikhand({
+  subsets: ["latin-ext"],
+  weight: "400",
+  variable: "--font-shrikhand",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${jetbrains_mono.variable} ${shrikhand.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
