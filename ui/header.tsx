0b940df5845d62ec378377f6497ef8ff45952475
Greg Nagy <greg@dhamma.works>
2023-11-23 17:00:30 +0100

    refactor header background
    - use the same method in both cases
      - one big svg that scales uniformly
      - colorful triangles can be manipulated programmatically
    - encapsulat the selecting logic to hide this concern

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,57 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link'
 
 import styles from './header.module.scss'
 
-import Logo from './logo'
-import Polygon from './polygon'
 import TicketBanner from "../ui/ticket-banner"
-
-function DesktopHeader() {
-  return (
-    <header className={styles.desktop_header}>
-      <Polygon type="header-left" className={styles.left_polygon} />
-      <Link href="/" className={styles.logo}>
-        <Logo />
-      </Link>
-      <Polygon type="header-right" className={styles.right_polygon} />
-    </header>
-  )
-}
-
-function MobileHeader() {
-  return (
-    <header className={styles.mobile_header}>
-      <Polygon type="mobile-header" className={styles.mobile_polygon} />
-    </header>
-  )
-}
+import HeaderBackground from './header-background';
 
 function Header() {
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    // Function to update state based on screen size
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    // Check and set on mount (and if window is available)
-    if (typeof window !== 'undefined') {
-      handleResize();
-      window.addEventListener('resize', handleResize);
-    }
-
-    // Cleanup listener on unmount
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   return (
     <div className={styles.main}>
-      {/* {isMobile ? <MobileHeader /> : <DesktopHeader />} */}
-      <MobileHeader />
+      <HeaderBackground />
       <TicketBanner />
-      asdf
       {
         false && <div className={styles.cfp_container}>
           <Link href="/" >
