ac4cd9f752ff6ea723a616e8c27992c3afcccfb8
Greg Nagy <greg@dhamma.works>
2023-11-23 16:22:52 +0100

    use classname instead of element
    easier to understand what's happening

--- ui/ticket-banner.tsx
+++ ui/ticket-banner.tsx
@@ -12,7 +12,7 @@ function TicketBanner() {
                     Join us for a celebration of innovation, inspiration, and inclusive community.
                 </p>
             </div>
-            <Link href="https://ti.to/jsconf-bp/jsconf-budapest-2024" target='_blank'>
+            <Link href="https://ti.to/jsconf-bp/jsconf-budapest-2024" target='_blank' className={styles.button}>
                 Tickets
             </Link>
         </div>
