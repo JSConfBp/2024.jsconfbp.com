45af0fcf3def5caface719bb4d02d69db573e317
Greg Nagy <greg@dhamma.works>
2023-11-23 18:51:01 +0100

    cfp button refactor + responsive

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import Link from 'next/link'
 
 import styles from './header.module.scss'
 
 import TicketBanner from "../ui/ticket-banner"
 import HeaderBackground from './header-background';
+import CfpButton from './cfp-button';
 
 function Header() {
-
-
   return (
     <div className={styles.main}>
       <HeaderBackground />
       <TicketBanner />
-      {
-        false && <div className={styles.cfp_container}>
-          <Link href="/" >
-            Call for Papers
-          </Link>
-        </div>
-      }
+      {true && <CfpButton />}
     </div >
   )
 }
