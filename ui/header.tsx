7b9feef9461dcef33599277ac3a0b89157b24283
Greg Nagy <greg@dhamma.works>
2023-11-24 19:30:52 +0100

    mark header-background

--- ui/header.tsx
+++ ui/header.tsx
@@ -5,11 +5,14 @@ import styles from './header.module.scss'
 import TicketBanner from "../ui/ticket-banner"
 import HeaderBackground from './header-background';
 import CfpButton from './cfp-button';
+import Link from 'next/link';
 
 function Header() {
   return (
     <div className={styles.main}>
-      <HeaderBackground />
+      <Link href={typeof window !== 'undefined' ? window.location.href : "/"}>
+        <HeaderBackground />
+      </Link>
       <TicketBanner />
       {false && <CfpButton />}
     </div >
