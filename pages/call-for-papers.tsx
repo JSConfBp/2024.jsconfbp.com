7e09881e9ecdc5f5bc717d705a1397e5aef0ef2e
Greg Nagy <greg@dhamma.works>
2023-11-24 19:50:20 +0100

    scaffold cfp page

---
+++ pages/call-for-papers.tsx
@@ -0,0 +1,10 @@
+import Layout from "../ui/layout"
+
+
+function CallForPapers() {
+    return (
+        <Layout title="Sponsors" pathName="/call-for-papers" />
+    )
+}
+
+export default CallForPapers
