7ef88574a5b18c6aa1f04473313d8cbb749b6add
Greg Nagy <greg@dhamma.works>
2023-11-25 13:00:21 +0100

    refactor divider positioning

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -8,6 +8,7 @@ function Sponsors() {
             <Divider>
                 <h1>Sponsorship</h1>
             </Divider>
+            <div>Our common goal with every Sponsor is to make the experience even better for Attendees at the conference. We believe that Sponsor booths and activities could be part of the whole event, the hallway track, and even afterparties.</div>
         </Layout>
     )
 }
