6fc17e0905d3b00651ce7fa5fbfb548a45afb239
Krisztina Závecz <krisztina.zavecz@memoq.com>
2023-11-25 20:35:57 +0100

    generalize our button component: make url optional + allow to specify non-link children + use semantic button element

--- ui/cfp-button.tsx
+++ ui/cfp-button.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
 
 import styles from "./cfp-button.module.scss";
+import { ReactNode } from "react";
 
-function CfpButton() {
+interface CfpButtonProps {
+  url?: string;
+  children: ReactNode;
+}
+
+function CfpButton({url, children}: CfpButtonProps) {
     return (
         <div className={styles.main}>
-            <Link href="/" className={styles.button}>
-                Call for Papers
-            </Link>
-        </div>);
+          <button className={styles.button}>
+              {url ?
+                (<Link href="" className={styles.button}>
+                  {children}
+                </Link>)
+                : <>{children}</>
+              }
+          </button>
+        </div>)
 }
 
 export default CfpButton;
