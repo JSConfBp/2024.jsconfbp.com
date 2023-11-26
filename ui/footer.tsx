43951c7d1e3131be5c80264363a058b25204d98b
Greg Nagy <greg@dhamma.works>
2023-11-26 15:58:49 +0100

    add

--- ui/footer.tsx
+++ ui/footer.tsx
@@ -48,13 +48,13 @@ function FooterSeparatorPolygons() {
 export function Footer() {
     return (
         <>
-            <Divider type="sym" className={styles.divider_override}>
+            <Divider type="sym" className={styles.divider_override} aria-hidden="true">
                 {FooterSeparatorPolygons()}
             </Divider>
             <div className={styles.coc_box}>
                 JSConf Budapest welcomes everybody, please be nice to each other.
             </div>
-            <div className={styles.footer_menu}>
+            <section className={styles.footer_menu}>
                 <nav>
                     <ul>
                         <li><a href="/about">About Us</a></li>
@@ -63,8 +63,27 @@ export function Footer() {
                         <li><a href="/budapest-guide">Budapest Guide</a></li>
                     </ul>
                 </nav>
-                <FooterPolygons />
-            </div>
+                <FooterPolygons aria-hidden="true" />
+            </section>
+            <nav className={styles.past_years}>
+                <ul>
+                    <li>
+                        <a href="https://2015.jsconfbp.com" target="_blank">2015</a>
+                    </li>
+                    <li>
+                        <a href="https://2016.jsconfbp.com" target="_blank">2016</a>
+                    </li>
+                    <li>
+                        <a href="https://2017.jsconfbp.com" target="_blank">2017</a>
+                    </li>
+                    <li>
+                        <a href="https://2019.jsconfbp.com" target="_blank">2019</a>
+                    </li>
+                    <li>
+                        <a href="https://2022.jsconfbp.com" target="_blank">2022</a>
+                    </li>
+                </ul>
+            </nav>
         </>
     );
 }
