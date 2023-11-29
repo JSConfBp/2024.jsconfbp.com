c65024a9230bbef83f5214b8a51af68817a46843
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2023-11-29 22:07:18 +0100

    handle all social and content meta tags in one component

--- ui/social-share.tsx
+++ ui/social-share.tsx
@@ -25,15 +25,20 @@ function SocialShare({
   return (
     <Head>
       <title>{currentPageTitle}</title>
+      <link rel="canonical" href={url} />
       <meta name="description" content={metaDescription} key="description" />
       <meta property="og:title" content={currentPageTitle} key="og:title" />
       <meta property="og:description" content={metaDescription} key="og:description" />
       <meta property="og:image" content={socialImgAbsoluteUrl} key="og:image" />
+      <meta property="og:image:alt" content="JSConf Budapest June 26-28, 2024 logo" key="og:image:alt" />
+      <meta property="og:url" content={url} key="og:url" />
       <meta name="twitter:title" content={currentPageTitle} key="twitter:title" />
       <meta name="twitter:description" content={metaDescription} key="twitter:description" />
       <meta name="twitter:image" content={socialImgAbsoluteUrl} key="twitter:image" />
-      <link rel="canonical" href={url} />
-      <meta property="og:url" content={url} key="og:url" />
+      <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
+      <meta name="twitter:site" content="@jsconfbp" key="twitter:site" />
+      <meta name="twitter:creator" content="@_gregnagy" key="twitter:creator" />
+      <meta property="twitter:image:alt" content="JSConf Budapest June 26-28, 2024 logo" key="twitter:image:alt" />
     </Head>
   )
 }
