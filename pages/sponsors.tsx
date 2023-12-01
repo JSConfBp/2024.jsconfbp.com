f1d12432475f31101a677a6c9497b65d60934578
Greg Nagy <greg@dhamma.works>
2023-12-01 15:59:30 +0100

    reach block

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -9,6 +9,10 @@ import mic from "../public/icons/mic.png"
 import attendees from "../public/icons/attendees.png"
 import countries from "../public/icons/countries.png"
 
+import twitter from "../public/icons/twitter.png"
+import youtube from "../public/icons/youtube.png"
+import facebook from "../public/icons/facebook.png"
+
 function Sponsors() {
     return (
         <>
@@ -75,11 +79,46 @@ function Sponsors() {
                     </div>
                 </ColoredSection>
                 <div id="video">VIDEO</div>
-                <ColoredSection background="primary">
+                <ColoredSection background="primary" className="pb-2">
                     <div id="attendees">attendees</div>
                     <div id="top-countries">top countries</div>
                     <Divider type="sym" color="background" />
-                    <div id="reach">reach</div>
+                    <div id="reach" className="w-80vw col center gap-1p5">
+                        <h1 className="center color-bg">REACH</h1>
+                        <h3 className="center color-bg">100% relevance in the JavaScript community</h3>
+                        <div className="col row-md center gap-1p5 w-100p py-2">
+                            <div className="col center">
+                                <Image
+                                    src={twitter}
+                                    alt="microphone icon"
+                                />
+                                <div>
+                                    <p className="m-0 fw-700 color-bg fs-2p25">2.1k</p>
+                                    <p className="m-0 color-bg fs-1p25">followers</p>
+                                </div>
+                            </div>
+                            <div className="col center relative">
+                                <Image
+                                    src={youtube}
+                                    alt="attendees icon"
+                                />
+                                <p className="m-0 fw-700 color-bg fs-2p25">142k</p>
+                                <p className="m-0  color-bg fs-1p25">subscribers</p>
+                            </div>
+                            <div className="col center relative">
+                                <Image
+                                    src={facebook}
+                                    alt="countries icon"
+                                />
+                                <p className="m-0 fw-700 color-bg fs-2p25">34k</p>
+                                <p className="m-0 color-bg fs-1p25">people reached</p>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <p className="color-bg">Our videos were watched over 242,000 times on YouTube</p>
+                            <p className="color-bg">Viewers spend more than 1,742,000 minutes watching the talks online</p>
+                        </div>
+                    </div>
                 </ColoredSection>
                 <div id="pictures">PICTURES</div>
                 <ColoredSection background="secondary">
