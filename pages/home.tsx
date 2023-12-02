582cb3137c33c0bd74c8bbfcaaa59ce38074391f
Greg Nagy <greg@dhamma.works>
2023-12-02 14:26:53 +0100

    scaffold index copy
    so we can work on these parts without accidentally
    publishing WIP sections

---
+++ pages/home.tsx
@@ -0,0 +1,19 @@
+import styles from './home.module.scss'
+
+import CfpButton from '../ui/cfp-button'
+import SocialShare from '../ui/social-share'
+import TicketBanner from '../ui/ticket-banner'
+
+function Home() {
+  return (
+    <>
+      <SocialShare />
+      <TicketBanner />
+      <div className={styles.cfp_container}>
+        <CfpButton href="/call-for-papers" target="_self">Our CFP is open!</CfpButton>
+      </div>
+    </>
+  )
+}
+
+export default Home
