dda6dc69378a1078de9401f62e22f8916904a87c
Krisztina Závecz <krisztina.zavecz@memoq.com>
2023-12-21 11:23:27 +0100

    add sections, reorg & mobile-firstify layout

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -4,6 +4,8 @@ import classnames from 'classnames'
 import styles from './about.module.scss'
 
 import Youtube from '../ui/youtube'
+import ColoredSection from '../ui/colored-section'
+import { ColorNames } from './_app'
 
 const team = [
   {
@@ -52,96 +54,102 @@ const team = [
 function About() {
   return (
     <>
-      <h1>
-        SZIA! ПРИВІТ! CZEŚĆ! SALUT! ЋАО! AHOJ! DOBRÝ DEN! ΓΕΙΆ ΣΟΥ! HOI! BUNĂ!
-        ЗДРАВЕЙТЕ! ПРИВЕТ! HELLO!
-      </h1>
+      <section className='intro'>
+        <h1>
+          SZIA! ПРИВІТ! CZEŚĆ! SALUT! ЋАО! AHOJ! DOBRÝ DEN! ΓΕΙΆ ΣΟΥ! HOI! BUNĂ!
+          ЗДРАВЕЙТЕ! ПРИВЕТ! HELLO!
+        </h1>
 
-      <p>
-        We aim to be the JSConf of the region around Eastern-Middle Europe, an
-        area so diverse in culture, people and views. A strictly nonprofit,
-        open-sourced and community driven conference for the community of
-        JavaScript, with the purpose to build and educate about new
-        technologies, ideas and culture.
-      </p>
-
-
-      <Youtube videoId="WPj0fDUqt1M" title="JSConf Budapest 2022 Mood video" className="my-2" />
-
-      <Youtube videoId="oWmPuKDiRV4" title="JSConf Budapest 2019 Mood video" className="my-2" />
-
-      <Youtube videoId="mJoS_pLbiWc" title="JSConf Budapest 2016 Mood video" className="my-2" />
+        <p>
+          We aim to be the JSConf of the region around Eastern-Middle Europe, an
+          area so diverse in culture, people and views. A strictly nonprofit,
+          open-sourced and community driven conference for the community of
+          JavaScript, with the purpose to build and educate about new
+          technologies, ideas and culture.
+        </p>
 
 
-      <h2>
-        JSConf Budapest so far
-      </h2>
+        <Youtube videoId="WPj0fDUqt1M" title="JSConf Budapest 2022 Mood video" className="my-2" />
 
-      <p>Check out our earlier events!</p>
+        <Youtube videoId="oWmPuKDiRV4" title="JSConf Budapest 2019 Mood video" className="my-2" />
 
-      <h3>2022</h3>
+        <Youtube videoId="mJoS_pLbiWc" title="JSConf Budapest 2016 Mood video" className="my-2" />
+      </section>
 
-      <p>
-        <a href="https://2022.jsconfbp.com">Site & Speakers</a>,<br />
-        <a href="https://www.youtube.com/watch?v=SdV9Xy0E4CM&list=PL37ZVnwpeshGuMZrOZzEo8QLBjjpbtBGm">
-          Videos of 2022
-        </a>
-      </p>
 
-      <h3>2019</h3>
+      <ColoredSection className='jsconf-earlier' background='light-section'>
+        <h1>
+          JSConf Budapest so far
+        </h1>
 
-      <p>
-        <a href="https://2019.jsconfbp.com">Site & Speakers</a>,<br />
-        <a href="https://www.youtube.com/watch?v=OkovPZ1mVBI&list=PL37ZVnwpeshEMCvdYDdZ09Sy-toTftWu0">
-          Videos of 2019
-        </a>
-      </p>
+        <p>Check out our earlier events!</p>
 
-      <h3>2017</h3>
+        <h3>2022</h3>
 
-      <p>
-        <a href="http://2017.jsconfbp.com">Site & Speakers</a>,<br />
-        <a href="https://www.youtube.com/watch?v=aHVdJ0QT5rM&list=PL37ZVnwpeshH9ztZfHqvQhF1_Zm4VcCTD">
-          Videos of 2017
-        </a>
-      </p>
+        <p>
+          <a href="https://2022.jsconfbp.com">Site & Speakers</a>,<br />
+          <a href="https://www.youtube.com/watch?v=SdV9Xy0E4CM&list=PL37ZVnwpeshGuMZrOZzEo8QLBjjpbtBGm">
+            Videos of 2022
+          </a>
+        </p>
 
-      <h3>2016</h3>
+        <h3>2019</h3>
 
-      <p>
-        <a href="http://2016.jsconfbp.com">Site & Speakers</a>,<br />
-        <a href="https://www.youtube.com/watch?v=CL8_nlqTcw0&list=PL37ZVnwpeshE6PbF5GB4hvtU3A4HqMfxf">
-          Videos of 2016
-        </a>
-      </p>
+        <p>
+          <a href="https://2019.jsconfbp.com">Site & Speakers</a>,<br />
+          <a href="https://www.youtube.com/watch?v=OkovPZ1mVBI&list=PL37ZVnwpeshEMCvdYDdZ09Sy-toTftWu0">
+            Videos of 2019
+          </a>
+        </p>
 
-      <h3>2015</h3>
+        <h3>2017</h3>
 
-      <p>
-        <a href="http://2015.jsconfbp.com">Site & Speakers</a>,<br />
-        <a href="https://www.youtube.com/watch?v=1zj7M1LnJV4&list=PLFZ5NyC0xHDaaTy6tY9p0C0jd_rRRl5Zm">
-          Videos of 2015
-        </a>
-      </p>
+        <p>
+          <a href="http://2017.jsconfbp.com">Site & Speakers</a>,<br />
+          <a href="https://www.youtube.com/watch?v=aHVdJ0QT5rM&list=PL37ZVnwpeshH9ztZfHqvQhF1_Zm4VcCTD">
+            Videos of 2017
+          </a>
+        </p>
 
-      <h2>
-        The Team
-      </h2>
+        <h3>2016</h3>
 
-      <ul className={classnames('reset', styles.team)}>
-        {team.map((member) => {
-          const href = member.url || `https://twitter.com/${member.twitter}`
-          return (<li key={member.name}>
-            <a href={href} target='_blank'>
-              <span>
-                <img alt={member.name} src={member.image} />
-              </span>
-              <span className={styles.name} dangerouslySetInnerHTML={{ __html: member.name }}></span>
-            </a>
-          </li>
-          )
-        })}
-      </ul>
+        <p>
+          <a href="http://2016.jsconfbp.com">Site & Speakers</a>,<br />
+          <a href="https://www.youtube.com/watch?v=CL8_nlqTcw0&list=PL37ZVnwpeshE6PbF5GB4hvtU3A4HqMfxf">
+            Videos of 2016
+          </a>
+        </p>
+
+        <h3>2015</h3>
+
+        <p>
+          <a href="http://2015.jsconfbp.com">Site & Speakers</a>,<br />
+          <a href="https://www.youtube.com/watch?v=1zj7M1LnJV4&list=PLFZ5NyC0xHDaaTy6tY9p0C0jd_rRRl5Zm">
+            Videos of 2015
+          </a>
+        </p>
+      </ColoredSection>
+
+      <section>
+        <h1>
+          The Team
+        </h1>
+        <ul className={classnames('reset', styles.team)}>
+          {team.map((member) => {
+            const href = member.url || `https://twitter.com/${member.twitter}`
+            const hrefDisplay = member.url || `@${member.twitter}`
+            return (<li key={member.name}>
+              <img alt={member.name} src={member.image} />
+              <div className={styles["member-info"]}>
+                <h2 className='my-0' dangerouslySetInnerHTML={{ __html: member.name }}></h2>
+                <p className={styles["member-intro"]} dangerouslySetInnerHTML={{ __html: member.memberIntro }}></p>
+                <a href={href} target='_blank'>{hrefDisplay}</a>
+              </div>
+            </li>
+            )
+          })}
+        </ul>
+      </section>
     </>
   )
 }
