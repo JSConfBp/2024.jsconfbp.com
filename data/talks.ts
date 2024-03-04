6b2a3fbdaa3082c8b03d9bd128648328b02ec3b9
Greg Nagy <greg@dhamma.works>
2024-03-04 17:16:40 +0100

    delete bad copypaste

--- data/talks.ts
+++ data/talks.ts
@@ -59,47 +59,6 @@ const TALKS = [
       pronouns: "he/him",
       profilePicturePath: "/speakers/kilian.png",
       bio: "Kilian is a front-end developer with over 20 years of experience that switched from building websites to building apps to build websites with. He is interested in modern web development, desktop app development and new technologies, and regularly speaks about topics like responsive websites, design systems and Electron. Kilian is a frequent open source contributor.",
-      location: "s talk about how signals provides a better mental model to render user interfaces, and how they make developing apps more predictable and, likely, error-free.",
-      youtubeId: "",
-      flickrAlbumId: "",
-    },
-  },
-  {
-    published: true,
-    slug: "aakansha-doshi",
-    speaker: {
-      name: "Aakansha Doshi",
-      pronouns: "she/her",
-      profilePicturePath: "/speakers/aakansha.png",
-      bio: "Aakansha is passionate about Free and open-source software (FOSS) and loves deep diving into the internals of Javascript and React. Being one of the core maintainers of Excalidraw, she spends most of the time with Canvas. She leads the FOSS meetups in Bangalore, India. Beyond her tech interests, she is a big foodie, loves traveling, and spending quality time with her family.",
-      location: "India, Bangalore",
-      socialMediaHandle: "@aakansha1216",
-      socialMediaUrl: "https://twitter.com/aakansha1216",
-      socialMediaType: "twitter",
-      website: "https://aakansha.dev/",
-      work: "Excalidraw",
-      role: "",
-      workURL: "",
-    },
-    talk: {
-      title: "Encrypting data in the Browser - Exploring Web Crypto APIs",
-      abstract: `Have you ever considered securing your app data within the browser? Let’s understand how encrypting your data in browser enhances overall security.
-
-        How do Web Crypto APIs differ from the Crypto module in Node.js?
-
-        In this talk I will be deep-diving into the world of Web Crypto APIs, starting from key generation, various encryption algorithms, implementation techniques, and more. The session will include live demos, coding examples, and a fun side project that I created as a result of learning about the Web Crypto APIs. Let’s set out on a collaborative learning adventure and delve into the functionalities of Web Crypto APIs.`,
-      youtubeId: "",
-      flickrAlbumId: "",
-    },
-  },
-  {
-    published: false,
-    slug: "kilian-valkhof",
-    speaker: {
-      name: "Kilian Valkhof",
-      pronouns: "he/him",
-      profilePicturePath: "/speakers/kilian.png",
-      bio: "Kilian is a front-end developer with over 20 years of experience that switched from building websites to building apps to build websites with. He is interested in modern web development, desktop app development and new technologies, and regularly speaks about topics like responsive websites, design systems and Electron. Kilian is a frequent open source contributor.",
       location: "Zoetermeer in The Netherlands",
       socialMediaHandle: "@kilian",
       socialMediaUrl: "https://twitter.com/kilian",
