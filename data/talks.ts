9de6f006e78b39e7f8047243d758b3c333fb453b
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2024-03-12 10:38:55 +0100

    fix path typo

--- data/talks.ts
+++ data/talks.ts
@@ -382,7 +382,7 @@ We'll learn about the history of programming languages, why type systems exist, 
     talk: {
       title: "Backend Essentials for Frontend Developers",
       abstract: `With the rise of full-stack frameworks and the blurred line between frontend and backend, it's becoming more and more important to have a good understanding of backend mechanics. In this talk, we'll be diving into some key backend concepts like webhooks, background jobs, cron jobs, and more. These concepts are really important for front-end developers, especially as their applications start getting more complex. This talk will provide you with guidance on when, how, and why you should integrate these components into your projects.`,
-      socialShareImage: "solcial-share/aleksandra.png",
+      socialShareImage: "social-share/aleksandra.png",
       youtubeId: "",
       flickrAlbumId: "",
     },
