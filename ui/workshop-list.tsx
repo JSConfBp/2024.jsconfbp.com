5db91be29d8b1e93a87ac845985de40326b53477
Greg Nagy <greg@dhamma.works>
2024-05-05 08:31:26 +0200

    add dynamic seatcount to the workshops page

--- ui/workshop-list.tsx
+++ ui/workshop-list.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import styles from "./workshop-list.module.scss";
 
 import WORKSHOPS from "../data/workshops";
-import CenteredButton from "./centered-button";
 import Polygons from "./polygons";
+import { SeatCount } from "./seat-count";
 
 type WorkshopListProps = {
   workshops: typeof WORKSHOPS;
@@ -12,7 +12,7 @@ type WorkshopListProps = {
 function WorkshopList({ workshops }: WorkshopListProps) {
   return (
     <div className={`md-row mb-1 mt-4 ${styles["workshop-list"]}`}>
-      {workshops.map(({ slug, title, summary, seats }, i) => {
+      {workshops.map(({ slug, title, summary, sponsor }, i) => {
         return (
           <div className={`${styles["workshop-list-item"]}`} key={slug}>
             <h3 className={styles.title}>
@@ -27,7 +27,9 @@ function WorkshopList({ workshops }: WorkshopListProps) {
             >
               <div className={styles["metadata"]}>
                 <span className={styles.date}>June 26th</span>
-                <span className={styles.seats}>Seats: {seats}</span>
+                <span className={styles.seats}>
+                  Seats: <SeatCount workshopKey={sponsor.name.toUpperCase()} />{" "}
+                </span>
                 <Polygons type="workshop-decor" className={styles.decor} />
               </div>
             </Link>
